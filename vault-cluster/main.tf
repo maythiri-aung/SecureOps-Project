690e07eb3642c3e49e305d6f74afa0a21e9b0c4b
PONE <user@Pone-Mac.local>
2024-09-15 16:36:07 +0900

    add vault terrafrom files

---
+++ vault-cluster/main.tf
@@ -0,0 +1,13 @@
+resource "hcp_hvn" "vault_hvn" {
+  hvn_id         = var.hvn_id #"cohort7"
+  cloud_provider = "aws"
+  region         = "us-southeast-1"
+  cidr_block     = "172.25.16.0/20"
+}
+
+resource "hcp_vault_cluster" "vault_cluster" {
+  cluster_id = "cohort7-vault-cluster"
+  hvn_id     = hcp_hvn.vault_hvn.hvn_id
+  tier       = "starter_small"
+  public_endpoint = true
+}
---
+++ vault-cluster/variables.tf
@@ -0,0 +1,5 @@
+variable "hvn_id" {
+  type        = string
+  description = "This is hvn id"
+  default = "cohort7"
+}
